6f5f3f37d5bb755c89ae241cc2c5a5d1dc4d21d7
Guilherme Rera <guilhermerera@gmail.com>
2024-04-23 11:56:24 -0300

    add: onesignal to project

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from "react-native-onesignal"
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("f0739497-2080-4939-876f-76e44a278250")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
