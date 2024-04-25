6644650e6e156c63f0afefc6afb56a59ad53e6da
Guilherme Rera <guilhermerera@gmail.com>
2024-04-25 00:53:01 -0300

    add notification permission request

--- App.tsx
+++ App.tsx
@@ -1,30 +1,35 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from "react-native-onesignal"
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_700Bold
+} from "@expo-google-fonts/roboto";
+import { OneSignal } from "react-native-onesignal";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
 
-OneSignal.initialize("f0739497-2080-4939-876f-76e44a278250")
+OneSignal.initialize("f0739497-2080-4939-876f-76e44a278250");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <CartContextProvider>
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </CartContextProvider>
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider theme={THEME}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			<CartContextProvider>
+				{fontsLoaded ? <Routes /> : <Loading />}
+			</CartContextProvider>
+		</NativeBaseProvider>
+	);
 }
