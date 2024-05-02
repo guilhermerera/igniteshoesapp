9505e1a02c17d0613a41dc495f4bc8761180866d
Guilherme Rera <guilhermerera@gmail.com>
2024-05-02 00:35:55 -0300

    add: setup to handle system notification click

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
 	Roboto_400Regular,
 	Roboto_700Bold
 } from "@expo-google-fonts/roboto";
-import { OneSignal } from "react-native-onesignal";
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal";
 
 import { Routes } from "./src/routes";
 
@@ -14,13 +14,30 @@ import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 import { tagUserEmaiLCreate } from "./src/notifications/notificationsTags";
+import { useEffect } from "react";
 
 OneSignal.initialize("f0739497-2080-4939-876f-76e44a278250");
 OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-	tagUserEmaiLCreate("guilhermerera@email.com")
+
+	tagUserEmaiLCreate("guilhermerera@email.com");
+
+	useEffect(() => {
+		const handleNotificationClick = (event: NotificationClickEvent): void => {
+			console.log("CLICOU")
+			console.log(event);
+		};
+
+		OneSignal.Notifications.addEventListener("click", handleNotificationClick);
+
+		return () =>
+			OneSignal.Notifications.removeEventListener(
+				"click",
+				handleNotificationClick
+			);
+	}, []);
 	return (
 		<NativeBaseProvider theme={THEME}>
 			<StatusBar
